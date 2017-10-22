338d20684a84422267734be221b024a3e72647e5
Matthias Derer <matthias.derer@esono.de>
2017-10-22 18:39:15 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# bmpwriter
+
+This little program is just there to experiment with writing files byte by byte using go. It writes the header and data of a simple bitmap file. As a reference [wikipedia](https://en.wikipedia.org/wiki/BMP_file_format) was used.
+
+The header struct looks like this:
+
+```go
+type BitmapHeader struct {
+	FileType                [2]byte
+	FileSize                uint32
+	Reserved1               uint16
+	Reserved2               uint16
+	BitmapOffset            uint32
+	SizeOfBitmapHeader      uint32
+	PixelWidth              uint32
+	PixelHeight             uint32
+	Planes                  uint16
+	BitsPerPixel            uint16
+	Compression             uint32
+	ImageSize               uint32
+	HorizontalResolution    uint32
+	VerticalResolution      uint32
+	NumberOfColorsInPalette uint32
+	ImportantColors         uint32
+}
+```
+
+It is written by the `binary` package directly to disk:
+
+```go
+binary.Write(f, binary.LittleEndian, header)
+binary.Write(f, binary.LittleEndian, data)
+```
+
+Please note that I tried to avoid the use of the `unsafe` package, so no `SizeOf` was available to me. Therefore lines like this exist:
+
+```go
+header.BitmapOffset = 40 + 14
+```
+
+The numbers are based on the types included in the header and have been counted by hand. This is not such a big problem in this case as the header spec for the bitmap format won't change. Even the standard library does it like this. See `writer.go` in the `image`package: [](https://github.com/golang/image/blob/master/bmp/writer.go)
+
+The program will write a `test.bmp`file with the result into the current working directory.
+
---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"encoding/binary"
+	"fmt"
+	"os"
+)
+
+// BitmapHeader represents the header fields needed for the bitmap format
+type BitmapHeader struct {
+	FileType                [2]byte
+	FileSize                uint32
+	Reserved1               uint16
+	Reserved2               uint16
+	BitmapOffset            uint32
+	SizeOfBitmapHeader      uint32
+	PixelWidth              uint32
+	PixelHeight             uint32
+	Planes                  uint16
+	BitsPerPixel            uint16
+	Compression             uint32
+	ImageSize               uint32
+	HorizontalResolution    uint32
+	VerticalResolution      uint32
+	NumberOfColorsInPalette uint32
+	ImportantColors         uint32
+}
+
+func main() {
+	var ImageWidth uint32 = 1024
+	var ImageHeight uint32 = 768
+
+	var x, y uint32
+
+	var numberOfPixels = ImageHeight * ImageWidth
+	var data = make([]uint32, numberOfPixels)
+	var c = 0
+	for y = 0; y < ImageHeight; y++ {
+		for x = 0; x < ImageWidth; x++ {
+			data[c] = 0x000000FF
+			c++
+		}
+	}
+
+	header := BitmapHeader{}
+
+	var SizeOfData = uint32(4 * numberOfPixels)
+
+	header.FileType = [2]byte{'B', 'M'}
+	header.FileSize = 40 + 14 + SizeOfData
+	header.BitmapOffset = 40 + 14
+	header.SizeOfBitmapHeader = 40
+	header.PixelWidth = ImageWidth
+	header.PixelHeight = ImageHeight
+	header.Compression = 0
+	header.BitsPerPixel = 32
+	header.Planes = 1
+	header.ImageSize = SizeOfData
+	header.NumberOfColorsInPalette = 0
+	header.ImportantColors = 0
+
+	fmt.Println(header.FileSize)
+
+	var cwd, err = os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	f, err := os.Create(cwd + "/test.bmp")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	binary.Write(f, binary.LittleEndian, header)
+	binary.Write(f, binary.LittleEndian, data)
+}
